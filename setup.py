79546bf6e3c43e0ac1930fddb324506d4fe64366
Jude <judith.clymo@gmail.com>
2023-12-08 16:06:07 +0000

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prioprom',
+    version='0.1',
+    description='Prediction priority promotions to solve parity games.',
+    packages=['prioprom']
+)
